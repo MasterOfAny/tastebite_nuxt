2be2eb9ad804bbf2b3ff07568dcd1fcba8fbc12d
Nik <77840519+MasterOfAny@users.noreply.github.com>
2024-11-13 13:22:10 +0300

    just commit

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -66,6 +66,7 @@
 </template>
 
 <script setup lang="ts">
+console.log(import.meta.env.API_BASE_URL)
 import Card from "@/components/ui/Card.vue"
 import processLink from "@/composables/processLink";
 import Newsletter from "~/components/functional/Newsletter.vue";
